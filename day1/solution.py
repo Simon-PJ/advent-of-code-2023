01dd3dd7506f02c7bcb6ace98b46b219b98b3c19
Simon Jones <simon_jones@waters.com>
2023-12-01 15:37:05 +0000

    day 1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -1,7 +1,16 @@
 input_lines = open("input.txt", "r").readlines()
 
+word_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 def get_calibration_value(line):
-    all_digits = list(filter(lambda c: c.isdigit(), line))
+    all_digits = []
+    for i in range(len(line)):
+        for word in word_digits:
+            if ''.join(line[i:]).startswith(word):
+                all_digits.append(str(word_digits.index(word) + 1))
+        if line[i].isdigit():
+            all_digits.append(line[i])
+
     usable_digits = all_digits[0] + all_digits[0 if len(all_digits) == 1 else len(all_digits) - 1]
     return int(usable_digits)
 
