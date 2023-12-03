24321f5c99c2f909188cbe2f742b0d37ee0b57d6
Simon Jones <simonp.jones@ntlworld.com>
2023-12-03 15:23:27 +0000

    Day 3 part one

---
+++ day3/AdventOfCodeDay3/Program.cs
@@ -0,0 +1,46 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var sumOfPartNumbers = 0;
+
+for (var j = 0; j < input.Length; j++)
+{
+    var number = "";
+    var isPartNumber = false;
+    for (var i = 0; i < input[0].Length; i++)
+    {
+        if (char.IsDigit(input[j][i]))
+        {
+            number += input[j][i];
+
+            if (!isPartNumber)
+            {
+                for (var i2 = -1; i2 <= 1; i2++)
+                {
+                    for (var j2 = -1; j2 <= 1; j2++)
+                    {
+                        var offseti = i + i2;
+                        var offsetj = j + j2;
+                        if (offseti >= 0 && offseti < input[0].Length && offsetj >= 0 && offsetj < input.Length && input[offsetj][offseti] != '.' && !char.IsDigit(input[offsetj][offseti]))
+                        {
+                            isPartNumber = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (!char.IsDigit(input[j][i]) || i + 1 == input[0].Length)
+        {
+            if (isPartNumber)
+            {
+                sumOfPartNumbers += int.Parse(number);
+            }
+
+            number = "";
+            isPartNumber = false;
+        }
+    }
+}
+
+Console.WriteLine(sumOfPartNumbers);
+Console.ReadKey();
