5647dbcbb6d4c2b364d33bc95d6eef15ed1213f7
Simon Jones <simon_jones@waters.com>
2023-12-06 14:51:21 +0000

    day 6 part 2

--- day6/solution.py
+++ day6/solution.py
@@ -2,25 +2,17 @@ from functools import reduce
 
 input_lines = open("input.txt", "r").readlines()
 
-def get_line_values(line):
-    return list(map(int, (filter(lambda x: x != "", line.strip().split(":")[1].split(' ')))))
+def get_line_value(line):
+    return int("".join(list(filter(lambda x: x != "", line.strip().split(":")[1].split(' ')))))
 
-times = get_line_values(input_lines[0])
-distances = get_line_values(input_lines[1])
+time = get_line_value(input_lines[0])
+record = get_line_value(input_lines[1])
 
-possible_records = []
-for race in range(len(times)):
-    record = distances[race]
-    time = times[race]
+possible_records = 0
+for i in range(1, time):
+    distance = (time - i) * i
+    if distance > record:
+        possible_records+=1
 
-    race_possible_records = 0
-    for i in range(1, time + 1):
-        distance = (time - i) * i
-        if distance > record:
-            race_possible_records+=1
-
-    possible_records.append(race_possible_records)
-
-sum_of_possible_records = reduce(lambda x, y: x*y, possible_records)
-print(sum_of_possible_records)       
+print(possible_records)
         
