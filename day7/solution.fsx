9eb0582090d697d457614f7aec28d4e7660db367
Simon Jones <simon_jones@waters.com>
2023-12-08 12:06:11 +0000

    day 7 part 2

--- day7/solution.fsx
+++ day7/solution.fsx
@@ -4,39 +4,45 @@ let input = List.ofSeq(System.IO.File.ReadLines("input.txt"))
 
 type HandBidAndScore = { hand: string; bid: int; score: int }
 
-let numberOfMatchingCards (card: char) (hand: string)=
-    Seq.filter (fun x -> card = x) hand |> Seq.length
+let numberOfMatchingCards (card: char) (hand: string) (includeJokers: bool)=
+    Seq.filter (fun x -> card = x || (includeJokers && x = 'J')) hand |> Seq.length
 
-let xOfAKind (hand: string) (x: int) = 
-    let maxOfAKind = Seq.map (fun x -> numberOfMatchingCards x hand) hand |> Seq.max
+let xOfAKind (hand: string) (x: int) (includeJokers: bool) = 
+    let maxOfAKind = Seq.map (fun x -> numberOfMatchingCards x hand includeJokers) hand |> Seq.max
     maxOfAKind = x
 
 let fiveOfAKind (hand: string) =
-    xOfAKind hand 5
+    xOfAKind hand 5 true
 
 let fourOfAKind (hand: string) =
-    xOfAKind hand 4
+    xOfAKind hand 4 true
 
 let threeOfAKind (hand: string) =
-    xOfAKind hand 3
+    xOfAKind hand 3 true
 
-let xOfNumber (hand: string) (number: int) (x: int) =
+let xOfNumber (hand: string) (number: int) (x: int) (includeJokers: bool) =
     Seq.distinct hand
-    |> Seq.map (fun a -> numberOfMatchingCards a hand)
+    |> Seq.map (fun a -> numberOfMatchingCards a hand includeJokers)
     |> Seq.filter (fun a -> a = number)
-    |> Seq.length = x
+    |> Seq.length >= x
 
-let xPairs (hand: string) (x: int) =
-    xOfNumber hand 2 x
+let xPairs (hand: string) (x: int) (includeJokers: bool) =
+    xOfNumber hand 2 x includeJokers
 
 let fullHouse (hand: string) =
-    xOfNumber hand 3 1 && xOfNumber hand 2 1
+    let numJokers = Seq.filter (fun x -> x = 'J') hand |> Seq.length
+    if (numJokers = 0) then
+        xOfNumber hand 3 1 false && xOfNumber hand 2 1 false
+    elif (numJokers = 1) then
+        xPairs hand 2 false
+    else
+        false
 
 let twoPair (hand: string) =
-    xPairs hand 2
+    xPairs hand 2 false
 
 let onePair (hand: string) =
-    xPairs hand 1
+    xPairs hand 1 true
 
 let calculateScore (hand: string) =
     if fiveOfAKind hand then
@@ -60,7 +66,7 @@ let parseHandBidAndScore (line: string) =
     { hand = parts[0]; bid = int parts[1]; score = score }
 
 let alphabettyHand (hand: string) =
-    let pictureMap = Map.empty.Add('T', 'J').Add('J', 'K').Add('Q', 'L').Add('K', 'M').Add('A', 'N')
+    let pictureMap = Map.empty.Add('T', 'J').Add('J', '.').Add('Q', 'L').Add('K', 'M').Add('A', 'N')
     Seq.map (fun x -> if Char.IsDigit(x) then char (64 + int x - 48) else pictureMap[x]) hand
     |> fun x -> String.Join("", x)
 
