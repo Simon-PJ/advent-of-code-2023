7bd78936ed72c6cc4af6238dc33840489e0ad1c1
Simon Jones <simonp.jones@ntlworld.com>
2023-12-09 18:37:40 +0000

    day 9 part 1

---
+++ day9/Day9/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var parsedLines = input.Select(x => x.Split(" ").Select(int.Parse).ToList()).ToList();
+
+var result = parsedLines.Sum(GetNextInLine);
+
+int GetNextInLine(List<int> line)
+{
+    var nextLine = new List<int>();
+
+    for (var i = 0; i < line.Count - 1; i++)
+    {
+        nextLine.Add(line[i + 1] - line[i]);
+    }
+
+    if (nextLine.All(x => x == 0))
+    {
+        return line.Last();
+    }
+    else
+    {
+        return line.Last() + GetNextInLine(nextLine);
+    }
+}
+
+Console.ReadLine();
