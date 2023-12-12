9d7221f9f80406ff88edbefe1b955539d166980e
Simon Jones <simon_jones@waters.com>
2023-12-12 16:03:57 +0000

    day 12 part 1

---
+++ day12/Day12/Program.cs
@@ -0,0 +1,46 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+const char Unknown = '?';
+const char Operational = '.';
+const char Damaged = '#';
+
+var lines = input.Select(x =>
+{
+    var springs = x.Split(' ')[0];
+    var arrangement = x.Split(' ')[1].Split(',').Select(int.Parse).ToArray();
+    return new Line(springs, arrangement);
+}).ToArray();
+
+string[] Combos(string springs)
+{
+    if (!springs.Contains(Unknown))
+        return new[] { springs };
+
+    var firstUnknown = springs.IndexOf(Unknown);
+
+    var asOperational = springs.Substring(0, firstUnknown) + Operational + springs.Substring(firstUnknown + 1);
+    var asDamaged = springs.Substring(0, firstUnknown) + Damaged + springs.Substring(firstUnknown + 1);
+
+    return new[] { asOperational, asDamaged }.SelectMany(x => Combos(x)).ToArray();
+}
+
+bool Valid(string springs, int[] arrangement)
+{
+    var actualArrangement = springs.Split('.', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Length);
+    return Enumerable.SequenceEqual(actualArrangement, arrangement);
+}
+
+var allCombos = lines.Select(line =>
+{
+    var combos = Combos(line.Springs).ToArray();
+
+    var validCombos = combos.Where(x => Valid(x, line.Arrangement)).ToArray();
+
+    return validCombos.Length;
+}).ToArray();
+
+var sumCombos = allCombos.Sum();
+
+Console.ReadLine();
+
+record Line(string Springs, int[] Arrangement);
