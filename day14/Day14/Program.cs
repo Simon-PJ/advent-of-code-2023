2238df550f7f1f5721dc18433744143b249f0e47
Simon Jones <simon_jones@waters.com>
2023-12-15 15:26:51 +0000

    day 14 part 2 incomplete

--- day14/Day14/Program.cs
+++ day14/Day14/Program.cs
@@ -6,32 +6,140 @@ const char SquareRock = '#';
 
 var totalLoad = 0;
 
-for (var i = 0; i < input[0].Length; i++)
+string[] TiltNorth()
 {
-    var tiltedLine = input[0][i].ToString();
+    var tiltedGrid = new string[input.Length];
+    for (var j = 0; j < input.Length; j++) tiltedGrid[j] = "";
 
-    for (var j = 1; j < input.Length; j++)
+    for (var i = 0; i < input[0].Length; i++)
     {
-        if (input[j][i] == RoundRock && tiltedLine[j - 1] == Empty)
+        var tiltedLine = input[0][i].ToString();
+
+        for (var j = 1; j < input.Length; j++)
         {
-            var drop = tiltedLine.LastIndexOfAny(new[] { SquareRock, RoundRock });
-            tiltedLine = tiltedLine.Substring(0, drop + 1)
-                + input[j][i]
-                + tiltedLine.Substring(drop + 1);
+            if (input[j][i] == RoundRock && tiltedLine[j - 1] == Empty)
+            {
+                var drop = tiltedLine.LastIndexOfAny(new[] { SquareRock, RoundRock });
+                tiltedLine = tiltedLine.Substring(0, drop + 1)
+                    + input[j][i]
+                    + tiltedLine.Substring(drop + 1);
+            }
+            else
+            {
+                tiltedLine += input[j][i];
+            }
         }
-        else
+
+        for (var j = 0; j < input.Length; j++) tiltedGrid[j] += tiltedLine[j];
+    }
+
+    return tiltedGrid;
+}
+
+//string[] TiltSouth()
+//{
+//    var tiltedGrid = new string[input.Length];
+//    for (var j = 0; j < input.Length; j++) tiltedGrid[j] = "";
+
+//    for (var i = 0; i < input[0].Length; i++)
+//    {
+//        var tiltedLine = input[0][i].ToString();
+
+//        for (var j = inputl; j < input.Length; j++)
+//        {
+//            if (input[j][i] == RoundRock && tiltedLine[j - 1] == Empty)
+//            {
+//                var drop = tiltedLine.LastIndexOfAny(new[] { SquareRock, RoundRock });
+//                tiltedLine = tiltedLine.Substring(0, drop + 1)
+//                    + input[j][i]
+//                    + tiltedLine.Substring(drop + 1);
+//            }
+//            else
+//            {
+//                tiltedLine += input[j][i];
+//            }
+//        }
+
+//        for (var j = 0; j < input.Length; j++) tiltedGrid[j] += tiltedLine[j];
+//    }
+
+//    return tiltedGrid;
+//}
+
+string[] RotateLeft(string[] a, int d)
+{
+
+    Queue<string> queue = new Queue<string>(a);
+    Stack<string> stack = new Stack<string>();
+
+    while (d > 0)
+    {
+        stack.Push(queue.Dequeue());
+        queue.Enqueue(stack.Pop());
+        d--;
+    }
+
+    return queue.ToArray();
+}
+
+bool SameAs(string[] a, string[] b)
+{
+    for (var i = 0; i < a.Length; i++)
+    {
+        for (var j = 0; j < a[0].Length; j++)
         {
-            tiltedLine += input[j][i];
+            if (a[j][i] != b[j][i]) return false;
         }
     }
 
-    var lineLoad = tiltedLine.Select((x, i) =>
-    {
-        var wregr = x == RoundRock ? tiltedLine.Length - i : 0;
-        return x == RoundRock ? tiltedLine.Length - i : 0;
-    }).Sum();
+    return true;
+}
 
-    totalLoad += lineLoad;
+int iteration = 0;
+int cycles = 0;
+string[] lastEasty = input;
+bool exit = false;
+while (!exit)
+{
+    switch (iteration)
+    {
+        case 0:
+            // North
+            input = TiltNorth();
+            break;
+        case 1:
+            // West
+            input = RotateLeft(input, 3);
+            TiltNorth();
+            input = RotateLeft(input, 1);
+            break;
+        case 2:
+            // South
+            input = RotateLeft(input, 2);
+            TiltNorth();
+            input = RotateLeft(input, 2);
+            break;
+        case 3:
+            // East
+            input = RotateLeft(input, 1);
+            TiltNorth();
+            input = RotateLeft(input, 3);
+
+            if (SameAs(lastEasty, input))
+            {
+                exit = true;
+            }
+
+            lastEasty = input;
+            break;
+        default:
+            throw new Exception("Oops");
+    }
+    input = TiltNorth();
+
+    iteration++;
+    cycles++;
+    iteration = iteration > 3 ? 0 : iteration;
 }
 
 Console.ReadLine();
